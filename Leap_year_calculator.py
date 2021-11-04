52da79096e1b80b2a0e0260196d374543b21d335
Francesco Arcamone <50881097+FrancescoArky@users.noreply.github.com>
2021-11-04 16:54:02 +0100

    Update Leap_year_calculator.py

--- Leap_year_calculator.py
+++ Leap_year_calculator.py
@@ -1,33 +1,24 @@
-def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        print("Leap year.")
-      else:
-        print("Not leap year.")
-    else:
-      print("Leap year.")
-  else:
-    print("Not leap year.")
-
 def days_in_month(year, month):
-  isleap = True
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        print("Leap year.")
-      else:
-        isleap = False
-        print("Not leap year.")
-    else:
-      print("Leap year.")
+  if year < 0 or month < 1 or month > 12:
+    return "invalid input"
   else:
-    isleap = False
-    print("Not leap year.")
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if isleap == True:
-    month_days[1] = 29
-  return f"{month_days[month-1]}"
+    isleap = True
+    if year % 4 == 0:
+      if year % 100 == 0:
+        if year % 400 == 0:
+          print("Leap year.")
+        else:
+          isleap = False
+          print("Not leap year.")
+      else:
+        print("Leap year.")
+    else:
+      isleap = False
+      print("Not leap year.")
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if isleap == True:
+      month_days[1] = 29
+    return f"{month_days[month-1]}"
 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
